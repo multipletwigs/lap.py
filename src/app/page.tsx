8cd65532bbab97ed807de57ded052a6151b6518a
Zach Khong <zachkhong.workplace@gmail.com>
2023-09-16 14:32:00 +0800

    design inits

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-clamp">
+      <div className="flex flex-col">
+        <h1>邝立浩 / Zachary Khong</h1>
+        <p>Design Engineer</p>
+      </div>
+    </main>
+  );
+}
