37939398b699331fc93939e498ad94b5a05d258c
multipletwigs <zachkhong.workplace@gmail.com>
2025-11-19 23:00:15 +0800

    feat

--- src/app/layout-navigation-wrapper.tsx
+++ src/app/layout-navigation-wrapper.tsx
@@ -4,7 +4,6 @@ import { usePathname } from "next/navigation";
 import { LayoutNavigation } from "./layout-navigation";
 
 export function LayoutNavigationWrapper() {
-  const pathname = usePathname();
-  
-  return <LayoutNavigation pathname={pathname} />;
+
+  return <LayoutNavigation />;
 }
